35fdcea366f444effca808e33302d944599adb38
Marcone Rodrigues Urzedo <91166593+marconerodr@users.noreply.github.com>
2023-12-16 10:34:43 -0300

    continuação v1 54:32

--- usuarios/views.py
+++ usuarios/views.py
@@ -5,4 +5,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def usuarios(request):
-    return render(request, 'usuarios.html')
+    if request.method == "GET":
+    	return render(request, 'usuarios.html')
+    elif request.method == "POST":
+        nome = request.POST.get()
+        sobrenome = request.POST.get()
