fe97fb0b9f6d8f3679318bac2acaf35a8b10cf71
Marcone <marcone.rodrigues@live.com>
2024-04-06 17:15:13 -0300

    V9 - fiz a geração de relatório

--- servicos/views.py
+++ servicos/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from .forms import FormServico
-from django.http import HttpResponse
+from django.http import HttpResponse, FileResponse
 from .models import Servico
+from fpdf import FPDF
+from io import BytesIO
 # Create your views here.
 
 def novo_servico(request):
@@ -25,3 +27,35 @@ def listar_servico(request):
 def servico(request, identificador):
     servico = get_object_or_404(Servico, identificador=identificador)
     return render(request, 'servico.html', {'servico': servico})
+
+def relatorio(request, identificador):
+    servico = get_object_or_404(Servico, identificador=identificador)
+
+    pdf = FPDF()
+    pdf.add_page()
+
+    pdf.set_font('Arial', 'B', 10)
+
+    pdf.set_fill_color(240,240,240)
+    pdf.cell(35, 10, 'Usuário:', 1, 0, 'L', 1)
+    pdf.cell(35, 10, f'{servico.usuario}', 1, 1, 'L', 1)
+
+    pdf.cell(35, 10, 'Evoluções', 1, 0, 'L', 1)
+
+    categoria_beneficio = servico.categoria_beneficio.all()
+    for i, beneficio in enumerate(categoria_beneficio):
+        pdf.cell(0, 10, f'- {servico.titulo}', 1, 1, 'L', 1)
+        if not i==len(categoria_beneficio) -1:
+            pdf.cell(30, 10, '', 0, 0)
+    
+    pdf.cell(35, 10, 'Data de solicitação', 1, 0, 'L', 1)
+    pdf.cell(0, 10, f'{servico.data_solicitação}', 1, 1, 'L', 1)
+    pdf.cell(35, 10, 'Data de concessão', 1, 0, 'L', 1)
+    pdf.cell(0, 10, f'{servico.data_concessao}', 1, 1, 'L', 1)
+    pdf.cell(35, 10, 'Protocolo', 1, 0, 'L', 1)
+    pdf.cell(0, 10, f'{servico.protocolo}', 1, 1, 'L', 1)
+    
+    pdf_content = pdf.output(dest='S').encode('latin1')
+    pdf_bytes = BytesIO(pdf_content)
+
+    return FileResponse(pdf_bytes, as_attachment=True, filename=f"relatorio-{servico.protocolo}.pdf")
