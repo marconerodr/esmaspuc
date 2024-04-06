be45a475abfb4536cf321d38bce4418b28ff7b7a
Marcone <marcone.rodrigues@live.com>
2024-04-06 13:07:00 -0300

    V7 - inseri formulário para cadastrar um novo serviço realizado, usando django model forms

---
+++ servicos/forms.py
@@ -0,0 +1,20 @@
+from django.forms import ModelForm
+from .models import Servico, CategoriaBeneficio
+
+class FormServico(ModelForm):
+    class Meta:
+        model = Servico
+        exclude = ['finalizado', 'protocolo']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({'class': 'form-control'})
+            self.fields[field].widget.attrs.update({'placeholder': field})
+        
+        choices = list()
+        for i, j in self.fields['categoria_beneficio'].choices:
+            categoria = CategoriaBeneficio.objects.get(titulo=j)
+            choices.append((i.value, categoria.get_titulo_display()))
+        
+        self.fields['categoria_beneficio'].choices = choices
