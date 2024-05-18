602a404d5050caa27575e86204db8f8a2a37cb60
Marcone <marcone.rodrigues@live.com>
2024-05-18 17:57:14 -0300

    Inseri teste unitário na aplicação usuarios

--- usuarios/tests.py
+++ usuarios/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import TestCase, LiveServerTestCase
+from .models import PopulacaoUsuaria
 
 # Create your tests here.
+
+class PopulacaoUsuariaTestCase(TestCase):
+    def test_create_user(self):
+        # Cria um usuário
+        usuario = PopulacaoUsuaria.objects.create(nome='John', sobrenome='Doe', email='john.doe@example.com', cpf='12345678900')
+        
+        # Verifica se os atributos foram definidos corretamente
+        self.assertEqual(usuario.nome, 'John')
+        self.assertEqual(usuario.sobrenome, 'Doe')
+        self.assertEqual(usuario.email, 'john.doe@example.com')
+        self.assertEqual(usuario.cpf, '12345678900')
